84a1a110d7badd58375ded8aecf7be687b5afeab
Daisy Lu <daisylu94@gmail.com>
2020-06-13 17:23:52 -0500

    refactor to create base class for multithreaded requests

---
+++ aemo.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import datetime
+from scraper import GetData
+
+class AEMO(GetData):
+    def __init__(self, regions, years):
+        self.regions = regions
+        self.years = years
+        super().__init__()
+    
+    @property
+    def headers(self):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"
+        }
+        return headers
+    
+    def create_urls(self):
+        complete_data_urls = []
+        for region in self.regions:
+            for year in self.years:
+                for month in range(1, 13):
+                    month_str = str(month).zfill(2)
+                    year_month = str(year) + month_str
+                    url = f"https://www.aemo.com.au/aemo/data/nem/priceanddemand/PRICE_AND_DEMAND_{year_month}_{region}1.csv"
+                    complete_data_urls.append(url)
+        return complete_data_urls 
+
+    def format_date_string(self, date_str):
+        if date_str:
+            date_str = date_str.replace("\"", "")
+            date_str = datetime.datetime.strptime(date_str, "%Y/%m/%d %H:%M:%S")
+        else:
+            date_str = None
+        return date_str
+
+    def parse_aemo_data(self, resp_text):
+        # convert to df
+        data = pd.DataFrame([row.split(",") for row in resp_text.split("\r\n")])
+        final_df = data.iloc[1:].copy()
+        final_df.columns = data.iloc[0]
+
+        # fix datatypes
+        final_df["SETTLEMENTDATE"] = final_df["SETTLEMENTDATE"].map(self.format_date_string)
+        float_cols = ["TOTALDEMAND", "RRP"]
+        for col in float_cols:
+            final_df[col] = final_df[col].astype(float)
+
+        return final_df
+    
+    def get_aemo_data(self):
+        urls = self.create_urls()
+        results = super().multithread_download(urls, self.headers)
+        parsed_results = [self.parse_aemo_data(r) for r in results]
+        parsed_results = pd.concat(parsed_results).reset_index(drop=True)
+        return parsed_results
+
+if __name__ == "__main__":
+    years = range(2016, 2020)
+    regions = ["NSW", "QLD", "VIC", "SA", "TAS"]
+    
+    aemo = AEMO(regions, years)
+    aemo_data = aemo.get_aemo_data()
+    len(aemo.errors)
+    
+    aemo_data.head()
--- scraper.py
+++ scraper.py
@@ -1,49 +1,28 @@
 import requests
-import pandas as pd
 import concurrent.futures
+from functools import partial
 
-def create_aemo_urls(years, regions):
-    complete_data_urls = []
-    for region in regions:
-        for year in years:
-            for month in range(1, 13):
-                month_str = str(month).zfill(2)
-                year_month = str(year) + month_str
-                url = f"https://www.aemo.com.au/aemo/data/nem/priceanddemand/PRICE_AND_DEMAND_{year_month}_{region}1.csv"
-                complete_data_urls.append(url)
-    return complete_data_urls 
-    
-def get_aemo_consumption(data_link):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
-    }
-    resp = requests.get(data_link, headers=headers)
-    
-    if resp.status_code == 200:
-        data = resp.text
-        return data
-    else:
-        print(f"Error for {data_link}")
+class GetData:
+    def __init__(self):
+        self.errors = []
 
-def parse_aemo_consumption(resp_text):
-    data = pd.DataFrame([row.split(",") for row in resp_text.split("\r\n")])
-    final_df = data.iloc[1:]
-    final_df.columns = data.iloc[0]
-    return final_df
-    
-def multithread_download(urls):
-    MAX_THREADS = 300
-    threads = min(MAX_THREADS, len(urls))
-    
-    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-        results = executor.map(get_aemo_consumption, urls)
-    
-    results = list(results)
-    
-    return results
+    def download_data(self, url, headers):
+        resp = requests.get(url, headers = headers)
 
-if __name__ == "__main__":
-    aemo_urls = create_aemo_urls(range(2016,2020), ["NSW", "QLD", "VIC", "SA", "TAS"])
-    au_data = multithread_download(aemo_urls)
-    parsed_au_data = [parse_aemo_consumption(r) for r in au_data]
-    parsed_au_data = pd.concat(parsed_au_data).reset_index(drop=True)
+        if resp.status_code == 200:
+            data = resp.text
+            return data
+        else:
+            self.errors.append(url)
+    
+    def multithread_download(self, url_list, headers):
+        MAX_THREADS = 300
+        threads = min(MAX_THREADS, len(url_list))
+        download_data = partial(self.download_data, headers = headers)
+        
+        with concurrent.futures.ThreadPoolExecutor(max_workers = threads) as executor:
+            results = executor.map(download_data, url_list)
+
+        results = list(results)
+
+        return results
